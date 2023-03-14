caf2e2070902e735f36721fdf0e2458c955ebcfc
Mehak Imtiaz <mehak.imtiaz@redmath.com>
2023-03-14 13:15:35 +0500

    Added popup edit on text hover

--- src/TextAnnotator.js
+++ src/TextAnnotator.js
@@ -1,12 +1,11 @@
 import React from "react";
 import { TokenAnnotator } from "react-text-annotate";
 
-const TEXT =
-  "When Sebastian Thrun started working on self-driving cars at Google in 2007, few people outside of the company took him seriously. “I can tell you very senior CEOs of major American car companies would shake my hand and turn away because I wasn’t worth talking to,” said Thrun, now the co-founder and CEO of online higher education startup Udacity, in an interview with Recode earlier this week. A little less than a decade later, dozens of self-driving startups have cropped up while automakers around the world clamor, wallet in hand, to secure their place in the fast-moving world of fully automated transportation.";
 const TAG_COLORS = {
   ORG: "#00ffa2",
   PERSON: "#84d2ff",
-  HARD: "RED"
+  HARD: "RED",
+  CUSTOM: 'YELLOW'
 };
 
 const Card = ({ children }) => (
@@ -14,7 +13,8 @@ const Card = ({ children }) => (
     style={{
       boxShadow: "0 2px 4px rgba(0,0,0,.1)",
       margin: 6,
-      maxWidth: 500,
+      minWidth: 1000,
+      maxWidth: 1000,
       padding: 16
     }}
   >
@@ -33,7 +33,10 @@ class TextAnnotator extends React.Component {
       { start: 15, end: 20, tag: "PERSON" },
       { start: 71, end: 75, tag: "DATE" }
     ],
-    tag: "PERSON"
+    tag: "PERSON",
+    selectedText: '',
+    showEditPopup: false,
+    text: ''
   };
 
   handleChange = (value) => {
@@ -44,13 +47,77 @@ class TextAnnotator extends React.Component {
     this.setState({ tag: e.target.value });
   };
 
+  handleInputChange = (e) => {
+    this.setState({ text: e.target.value });
+  };
+  handleSelectedInputChange = (e) => {
+    this.setState({ selectedText: e.target.value });
+  };
+
+  handleMouseOut = (event) => {
+    this.setState({ showEditPopup: false });
+    event.target.removeEventListener('mouseout', this.handleMouseOut);
+  };
+
+  handleMouseOver = (e) => {
+    const text = window.getSelection().toString();
+    console.log("e inside mouse over", e)
+    if (text) {
+      this.setState({
+        selectedText: text
+      });
+      this.setState({ showEditPopup: true });
+      e.target.addEventListener('mouseout', this.handleMouseOut);
+    }
+  };
+
+  handleEdit = (e) => {
+    console.log('Edit clicked', e);
+  }
   render() {
     return (
-      <div style={{ padding: 24, fontFamily: "IBM Plex Sans" }}>
-        <div style={{ display: "flex", marginBottom: 24 }}>
+      <div div style = {
+        {
+          padding: 24,
+          fontFamily: "IBM Plex Sans"
+        }
+      } >
+        <div style = {
+          {
+            display: "flex",
+            marginBottom: 24
+          }
+        } >
           <Card>
-            <h4>Default</h4>
-            <select onChange={this.handleTagChange} value={this.state.tag}>
+            <h4>Text Annotator</h4>
+               <textarea value = {
+                 this.state.text
+               }
+               onChange = {
+                 this.handleInputChange
+               }
+               style = {
+                 {
+                   display: "flex",
+                   marginBottom: 15,
+                   width: 500,
+                   maxWidth: 500,
+                   height: "100px"
+                 }
+               }
+               />
+            <select select onChange = {
+              this.handleTagChange
+            }
+            value = {
+              this.state.tag
+            }
+            style = {
+              {
+                display: "flex",
+                marginBottom: 15
+              }
+            } >
               <option value="ORG">ORG</option>
               <option value="PERSON">PERSON</option>
               <option value="HARD">HARD SKILL</option>
@@ -58,18 +125,38 @@ class TextAnnotator extends React.Component {
             <TokenAnnotator
               style={{
                 fontFamily: "IBM Plex Sans",
-                maxWidth: 500,
+                minWidth: 1000,
+                maxWidth: 1000,
                 lineHeight: 1.5
               }}
-              tokens={TEXT.split(" ")}
-              value={this.state.value}
+              tokens={ this.state.text.split(" ")}
+              value={ this.state.value }
               onChange={this.handleChange}
+              onMouseOver={this.handleMouseOver}
               getSpan={(span) => ({
                 ...span,
                 tag: this.state.tag,
                 color: TAG_COLORS[this.state.tag]
               })}
             />
+            { this.state.showEditPopup &&
+            <div style={{ position: 'absolute', top: '10px', left: '50%', transform: 'translateX(-50%)', backgroundColor: 'white', border: '1px solid black', padding: '10px' }}>
+
+              <textarea value = {
+                this.state.selectedText
+              }
+              onChange={ this.handleSelectedInputChange }
+              style = {{
+                  display: "block",
+                  marginBottom: 15,
+                  width: 300,
+                  height: "100px"
+                }}
+              />
+              <button onClick={this.handleEdit}>Edit</button>
+              <p>{this.state.selectedText}</p>
+            </div>
+          }
           </Card>
         </div>
 
